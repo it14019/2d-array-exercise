255fe3583f33e316e87e7d60284b6ba6a8ce8dad
it14019 <razovicka@inbox.lv>
2020-07-30 16:33:23 +0300

    refractored with functions

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,18 +9,29 @@ namespace Task1
             int rowsLength = 20;
             int columnsLength = 20;
             int[,] numberArray = new int[rowsLength, columnsLength];
+
+            FillArray(numberArray);
+            FindMinMaxValues(numberArray);
+            SortByRows(numberArray);
+        }
+
+        public static void FillArray(int[,]numberArray)
+        {
             Random rnd = new Random();
- 
-            for (int r = 0; r < rowsLength; ++r)
+
+            for (int r = 0; r < numberArray.GetLength(0); ++r)
             {
-                for (int c = 0; c < columnsLength; ++c)
+                for (int c = 0; c < numberArray.GetLength(1); ++c)
                 {
-                    numberArray[r, c] = rnd.Next(1,400);     
+                    numberArray[r, c] = rnd.Next(1, 400);
                     Console.Write(numberArray[r, c] + "\t");
                 }
                 Console.WriteLine();
             }
+        }
 
+        public static void FindMinMaxValues(int[,] numberArray)
+        {
             int? minValue = null;
             int? minRow = null;
             int? minCol = null;
@@ -28,9 +39,9 @@ namespace Task1
             int? maxRow = null;
             int? maxCol = null;
 
-            for (int r = 0; r < rowsLength; ++r)
+            for (int r = 0; r < numberArray.GetLength(0); ++r)
             {
-                for (int c = 0; c < columnsLength; ++c)
+                for (int c = 0; c < numberArray.GetLength(1); ++c)
                 {
                     int cell = numberArray[r, c];
                     int maxCell = numberArray[r, c];
@@ -46,25 +57,28 @@ namespace Task1
                         maxValue = maxCell;
                         maxRow = r;
                         maxCol = c;
-                    }   
+                    }
                 }
             }
 
             if (minValue.HasValue && maxValue.HasValue)
             {
-                Console.WriteLine("\nMinimum: value[{0},{1}] is {2}", minRow, minCol, minValue);
-                Console.WriteLine("Maximum: value[{0},{1}] is {2}\n", maxRow, maxCol, maxValue);
+                Console.WriteLine("\nMin: value[{0},{1}] is {2}", minRow, minCol, minValue);
+                Console.WriteLine("Max: value[{0},{1}] is {2}\n", maxRow, maxCol, maxValue);
             }
             else
             {
                 Console.WriteLine("no value");
             }
+        }
 
-            for (int r = 0; r < rowsLength; r++)
+        public static void SortByRows(int[,] numberArray)
+        {
+            for (int r = 0; r < numberArray.GetLength(0); r++)
             {
-                for (int c = 0; c < columnsLength; c++)
+                for (int c = 0; c < numberArray.GetLength(1); c++)
                 {
-                    for (int k = 0; k < columnsLength - c - 1; k++)
+                    for (int k = 0; k < numberArray.GetLength(1) - c - 1; k++)
                     {
                         if (numberArray[r, k] > numberArray[r, k + 1])
                         {
@@ -76,9 +90,9 @@ namespace Task1
                 }
             }
 
-            for (int r = 0; r < rowsLength; r++)
+            for (int r = 0; r < numberArray.GetLength(0); r++)
             {
-                for (int c = 0; c < columnsLength; c++)
+                for (int c = 0; c < numberArray.GetLength(1); c++)
                     Console.Write(numberArray[r, c] + "\t");
                 Console.WriteLine();
             }
